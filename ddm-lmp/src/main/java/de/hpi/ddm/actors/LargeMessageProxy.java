ef5eee76ce8543d3ef4d826cb9b428635f028d94
Tom Lichtenstein <lichtenstein@uni-potsdam.de>
2019-11-10 17:02:28 +0100

    Refactor code and add unused kryo serialization

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -8,6 +8,7 @@ import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import com.esotericsoftware.kryo.Kryo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,21 +21,18 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 
 	@Data @NoArgsConstructor @AllArgsConstructor
-	private class LargeMessagePackageCollection {
+	private class LargeMessageBuffer {
 		public int packageCounter;
 		public int packageAmount;
 		public byte[][] payload;
 
-		public void incrementPackageCount(){
-			this.packageCounter++;
-		}
-
 		public void setPayload(int packageId, byte[] packageData){
 			this.payload[packageId] = packageData;
+			this.packageCounter++;
 		}
 	}
 
-	private HashMap<Integer, LargeMessagePackageCollection> largeMessageBuffer = new HashMap<>();
+	private HashMap<Integer, LargeMessageBuffer> largeMessageBuffer = new HashMap<>();
 
 	////////////////////////
 	// Actor Construction //
@@ -86,118 +84,169 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 				.build();
 	}
 
+	public static int byteArrayToInt(byte[] b) {
+		return b[3] & 0xFF | (b[2] & 0xFF) << 8 | (b[1] & 0xFF) << 16 | (b[0] & 0xFF) << 24;
+	}
+
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
 
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		ObjectOutput out = null;
-		byte[] bytes = null;
+		// Serialize Message
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutput objectOut = null;
+		byte[] messageBytes = null;
+
+		// Java Serializer
 		try {
-			out = new ObjectOutputStream(bos);
-			out.writeObject(message.getMessage());
-			out.flush();
-			bytes = bos.toByteArray();
-			System.out.println("Initial serialzed message length: " + bytes.length);
-		} catch(IOException e) {
+			objectOut = new ObjectOutputStream(baos);
+			objectOut.writeObject(message.getMessage());
+			objectOut.flush();
+			messageBytes = baos.toByteArray();
+		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				bos.close();
+				baos.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 		}
 
-		if (bytes == null) {
+		// Kryo Serializer - Not used because the result is 12,4 % bigger even with class registration
+		/*
+		Kryo kryo = new Kryo();
+		kryo.setRegistrationRequired(false);
+		Output output = new Output(baos);
+		kryo.writeObject(output, message.getMessage());
+		output.flush();
+		messageBytes = baos.toByteArray();
+		System.out.println(messageBytes.length);
+		output.close();
+		try {
+			baos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
+
+		if (messageBytes == null) {
 			return;
 		}
 
-		final int payloadBytes = 262144 - 12;
-		int packageCount = (int)Math.ceil(bytes.length / (double) payloadBytes);
-		int messageId = (int)Math.floor(Math.random() * Integer.MAX_VALUE);
-		System.out.println("Sender ID " + messageId);
-		System.out.println("Sender Package Count: " + packageCount);
+		// Maximal message size: 262144
+		final int metaInfoBytes = 12;
+		final int payloadBytes = 262000 - metaInfoBytes;
+
+		// Create package meta information (number of packages, package id)
+		int packageCount = (int) Math.ceil(messageBytes.length / (double) payloadBytes);
+		int messageId = (int) Math.floor(Math.random() * Integer.MAX_VALUE);
+
 		byte[] messageIdBytes = ByteBuffer.allocate(4).putInt(messageId).array();
 		byte[] packageCountBytes = ByteBuffer.allocate(4).putInt(packageCount).array();
-		for(int i = 0; i < packageCount; i++){
+
+		for (int i = 0; i < packageCount; i++) {
 			byte[] packageIdBytes = ByteBuffer.allocate(4).putInt(i).array();
-			byte[] dataBytes = Arrays.copyOfRange(bytes, (i*payloadBytes), Math.min(((i+1)*payloadBytes), bytes.length));
-			try{
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+
+			// Split data into chunks (packages)
+			byte[] dataBytes = Arrays
+					.copyOfRange(messageBytes, (i * payloadBytes), Math.min(((i + 1) * payloadBytes), messageBytes.length));
+			try {
+				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 				outputStream.write(messageIdBytes);
 				outputStream.write(packageCountBytes);
 				outputStream.write(packageIdBytes);
 				outputStream.write(dataBytes);
-				BytesMessage bm = new BytesMessage<>(outputStream.toByteArray(), this.sender(), message.getReceiver());
+				byte[] messagePackage = outputStream.toByteArray();
+
+				// Send chunk (package) to receiver
+				BytesMessage bm = new BytesMessage<>(messagePackage, this.sender(), message.getReceiver());
 				receiverProxy.tell(bm, this.self());
-			}catch(IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 		}
-
-
-	}
-
-	public static int byteArrayToInt(byte[] b) {
-		return b[3] & 0xFF | (b[2] & 0xFF) << 8 | (b[1] & 0xFF) << 16 | (b[0] & 0xFF) << 24;
 	}
 
 	private void handle(BytesMessage<?> message) {
 		// Collect data
 		byte[] messageData = (byte[])message.getBytes();
+
+		// Read meta information
 		int messageId = byteArrayToInt(Arrays.copyOfRange(messageData, 0, 4));
-		int packageCount = byteArrayToInt(Arrays.copyOfRange(messageData, 4, 8));
+		int numberOfPackages = byteArrayToInt(Arrays.copyOfRange(messageData, 4, 8));
 		int packageId = byteArrayToInt(Arrays.copyOfRange(messageData, 8, 12));
+
+		// Read payload
 		byte[] messagePayload = Arrays.copyOfRange(messageData, 12, messageData.length);
 
-		LargeMessagePackageCollection collection = largeMessageBuffer.get(messageId);
-		if(collection == null){
-			collection = new LargeMessagePackageCollection(0, packageCount, new byte[packageCount][]);
+		// Get collection for message id
+		LargeMessageBuffer collection = largeMessageBuffer.get(messageId);
+
+		// Init message buffer for new message id if it does not exist
+		if (collection == null) {
+			collection = new LargeMessageBuffer(0, numberOfPackages, new byte[numberOfPackages][]);
 			largeMessageBuffer.put(messageId, collection);
 		}
-		collection.incrementPackageCount();
+
+		// Add package to buffer
 		collection.setPayload(packageId, messagePayload);
 
-		if(collection.getPackageCounter() == packageCount) {
-			byte[] finalMessage = null;
+		// Check whether message is complete
+		if (collection.getPackageCounter() == numberOfPackages) {
+
+			// Remove buffer entry to free the message id for future messages
+			largeMessageBuffer.remove(messageId);
+
+			// Reassemble the message
+			byte[] completeMessage = null;
 			try {
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
 				for (byte[] entry : collection.getPayload()) {
-					outputStream.write(entry);
+					baos.write(entry);
 				}
-				finalMessage = outputStream.toByteArray();
+				completeMessage = baos.toByteArray();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
-			// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-			ByteArrayInputStream bis = new ByteArrayInputStream(finalMessage);
-			ObjectInput in = null;
-			Object o = null;
-			System.out.println(finalMessage.length);
+			// Reassemble the message content
+			ByteArrayInputStream bais = new ByteArrayInputStream(completeMessage);
+			Object messageObject = null;
+
+			// Java Deserializer
+			ObjectInput objectInput = null;
 			try {
-				in = new ObjectInputStream(bis);
-				o = in.readObject();
-
-				BytesMessage bm = new BytesMessage<>(o, this.sender(), message.getReceiver());
-				message.getReceiver().tell(bm.getBytes(), message.getSender());
-
-				//System.out.println(o.getClass().getName());
+				objectInput = new ObjectInputStream(bais);
+				messageObject = objectInput.readObject();
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (ClassNotFoundException f) {
 				f.printStackTrace();
 			} finally {
 				try {
-					if (in != null) {
-						in.close();
+					if (objectInput != null) {
+						objectInput.close();
 					}
 				} catch (IOException ex) {
-					// ignore close exception
+					// Ignore close exception
 				}
 			}
+
+			// Kryo Deserializer - Not used because the result is 12,4% bigger even with class registration
+			/*
+			Kryo kryo = new Kryo();
+			kryo.setRegistrationRequired(false);
+			Input input = new Input(bais);
+			messageObject = kryo.readClassAndObject(input);
+			input.close();
+			BytesMessage bm = new BytesMessage<>(o, this.sender(), message.getReceiver());
+			message.getReceiver().tell(bm.getBytes(), message.getSender());
+			*/
+
+			// Redirect message to actual target
+			BytesMessage bm = new BytesMessage<>(messageObject, this.sender(), message.getReceiver());
+			message.getReceiver().tell(bm.getBytes(), message.getSender());
 		}
 	}
 }
