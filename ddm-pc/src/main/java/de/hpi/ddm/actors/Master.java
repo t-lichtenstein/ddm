a576ee66d2667e9aa599afcfc7fdbabdfc7ac971
Tom Lichtenstein <lichtenstein@uni-potsdam.de>
2019-11-15 15:50:24 +0100

    Add password cracking algorithm and messaging behaviour

--- ddm-pc/src/main/java/de/hpi/ddm/actors/Master.java
+++ ddm-pc/src/main/java/de/hpi/ddm/actors/Master.java
@@ -1,9 +1,8 @@
 package de.hpi.ddm.actors;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
@@ -40,7 +39,12 @@ public class Master extends AbstractLoggingActor {
 	public static class StartMessage implements Serializable {
 		private static final long serialVersionUID = -50374816448627600L;
 	}
-	
+
+	@Data
+	public static class StartPollingCrackMessages implements Serializable {
+		private static final long serialVersionUID = -50374816448627601L;
+	}
+
 	@Data @NoArgsConstructor @AllArgsConstructor
 	public static class BatchMessage implements Serializable {
 		private static final long serialVersionUID = 8343040942748609598L;
@@ -51,14 +55,27 @@ public class Master extends AbstractLoggingActor {
 	public static class RegistrationMessage implements Serializable {
 		private static final long serialVersionUID = 3303081601659723997L;
 	}
+
+	@Data @AllArgsConstructor @NoArgsConstructor
+	public static class CrackMessage implements Serializable {
+		private static final long serialVersionUID = 3303081601659723998L;
+		private int id;
+		private String password;
+		private int length;
+		private char[] characters;
+		private String[] hints;
+		private int hintsToCrack;
+	}
 	
 	/////////////////
 	// Actor State //
 	/////////////////
-
 	private final ActorRef reader;
 	private final ActorRef collector;
 	private final List<ActorRef> workers;
+	private int passwordCounter = 0;
+	private final Queue<ActorRef> idleWorkers = new LinkedBlockingQueue<>();
+	private final Queue<CrackMessage> messages = new LinkedBlockingQueue<>();
 
 	private long startTime;
 	
@@ -80,6 +97,7 @@ public class Master extends AbstractLoggingActor {
 		return receiveBuilder()
 				.match(StartMessage.class, this::handle)
 				.match(BatchMessage.class, this::handle)
+				.match(Worker.CrackedPasswordMessage.class, this::handle)
 				.match(Terminated.class, this::handle)
 				.match(RegistrationMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
@@ -91,6 +109,18 @@ public class Master extends AbstractLoggingActor {
 		
 		this.reader.tell(new Reader.ReadMessage(), this.self());
 	}
+
+	private long recfact(long start, long n) {
+		long i;
+		if (n <= 16) {
+			long r = start;
+			for (i = start + 1; i < start + n; i++) r *= i;
+			return r;
+		}
+		i = n / 2;
+		return recfact(start, i) * recfact(start + i, n - i);
+	}
+	long factorial(long n) { return recfact(1, n); }
 	
 	protected void handle(BatchMessage message) {
 		
@@ -100,16 +130,44 @@ public class Master extends AbstractLoggingActor {
 		// 2. If we process the batches early, we can achieve latency hiding. /////////////////////////////////
 		// TODO: Implement the processing of the data for the concrete assignment. ////////////////////////////
 		///////////////////////////////////////////////////////////////////////////////////////////////////////
-		
+
 		if (message.getLines().isEmpty()) {
-			this.collector.tell(new Collector.PrintMessage(), this.self());
-			this.terminate();
+			this.reader.tell(PoisonPill.getInstance(), ActorRef.noSender());
+			// this.terminate();
 			return;
 		}
-		
-		for (String[] line : message.getLines())
-			System.out.println(Arrays.toString(line));
-		
+
+		List<String[]> lines = message.getLines();
+		passwordCounter += lines.size();
+		String[] firstEntry = lines.get(0);
+		final int firstHintIndex = 5;
+
+		char[] characters = firstEntry[2].toCharArray();
+		int characterAmount = firstEntry[2].length();
+		int passwordLength = Integer.parseInt(firstEntry[3]);
+		int numberOfHints = firstEntry.length - firstHintIndex;
+		int numberOfHintsToCrack = 0;
+		while (numberOfHintsToCrack < numberOfHints) {
+			if ((long) Math.pow((characterAmount - numberOfHintsToCrack), passwordLength) <
+					(factorial(characterAmount) / factorial(characterAmount - passwordLength - 1))) {
+				break;
+			}
+			numberOfHintsToCrack++;
+		}
+
+		for (String[] line : lines) {
+			int id = Integer.parseInt(firstEntry[0]);
+			String password = line[4];
+			String[] hints = Arrays.copyOfRange(line, firstHintIndex, line.length);
+			this.messages.add(new CrackMessage(id, password, passwordLength, characters, hints, numberOfHintsToCrack));
+		}
+
+		while (this.idleWorkers.size() > 0) {
+			this.idleWorkers
+					.remove()
+					.tell(this.messages.remove(), this.self());
+		}
+
 		this.collector.tell(new Collector.CollectMessage("Processed batch of size " + message.getLines().size()), this.self());
 		this.reader.tell(new Reader.ReadMessage(), this.self());
 	}
@@ -132,7 +190,30 @@ public class Master extends AbstractLoggingActor {
 	protected void handle(RegistrationMessage message) {
 		this.context().watch(this.sender());
 		this.workers.add(this.sender());
+		if (this.messages.size() != 0) {
+			this.sender().tell(this.messages.remove(), this.self());
+		} else {
+			this.idleWorkers.add(this.sender());
+		}
 //		this.log().info("Registered {}", this.sender());
+	}
+
+	protected void handle(Worker.CrackedPasswordMessage message) {
+		passwordCounter--;
+		this.collector.tell(new Collector.CollectMessage(
+				"ID: " + message.getId() + " | Password: " + message.getPassword()
+		), this.self());
+		if (passwordCounter == 0 && this.messages.size() == 0) {
+			this.collector.tell(new Collector.PrintMessage(), this.self());
+			this.terminate();
+			return;
+		} else {
+			if (this.messages.size() != 0) {
+				this.sender().tell(this.messages.remove(), this.self());
+			} else {
+				this.sender().tell(PoisonPill.getInstance(), ActorRef.noSender());
+			}
+		}
 	}
 	
 	protected void handle(Terminated message) {
--- ddm-pc/src/main/java/de/hpi/ddm/actors/Worker.java
+++ ddm-pc/src/main/java/de/hpi/ddm/actors/Worker.java
@@ -1,8 +1,12 @@
 package de.hpi.ddm.actors;
 
+import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import akka.actor.AbstractLoggingActor;
@@ -16,6 +20,10 @@ import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.Member;
 import akka.cluster.MemberStatus;
 import de.hpi.ddm.MasterSystem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import scala.Char;
 
 public class Worker extends AbstractLoggingActor {
 
@@ -37,13 +45,26 @@ public class Worker extends AbstractLoggingActor {
 	// Actor Messages //
 	////////////////////
 
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class CrackedPasswordMessage implements Serializable {
+		private static final long serialVersionUID = 3304081601659723998L;
+		private int id;
+		private String password;
+	}
+
 	/////////////////
 	// Actor State //
 	/////////////////
 
 	private Member masterSystem;
 	private final Cluster cluster;
-	
+	private List<Character> crackedHints = new ArrayList<>();
+	private List<String> hintHashes;
+	private boolean enoughHintsFound = false;
+	private String crackedPassword = "";
+
 	/////////////////////
 	// Actor Lifecycle //
 	/////////////////////
@@ -70,8 +91,44 @@ public class Worker extends AbstractLoggingActor {
 				.match(CurrentClusterState.class, this::handle)
 				.match(MemberUp.class, this::handle)
 				.match(MemberRemoved.class, this::handle)
+				.match(Master.CrackMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
+	}
+
+	private void handle(Master.CrackMessage message) {
+		// Phase 1: Hint cracking
+		this.hintHashes = new ArrayList(Arrays.asList(message.getHints()));
+		this.crackedHints = new ArrayList<>();
+		char[] alphabet = message.getCharacters();
+		heapPermutation(alphabet, alphabet.length, alphabet.length, message.getHintsToCrack());
+		this.enoughHintsFound = false;
+		this.log().debug("Cracked " + this.crackedHints.size() + " hints for password id: " + message.getId());
+
+		List<Character> passwordAlphabet = new ArrayList<>();
+
+		for (char character : alphabet) {
+			if (!this.crackedHints.contains(character)) {
+				passwordAlphabet.add(character);
+			}
+		}
+
+		// Phase 2: Crack password
+		this.crackedPassword = "";
+		generateCombinations(
+				passwordAlphabet,
+				message.getLength(),
+				"",
+				passwordAlphabet.size(),
+				message.getPassword());
+
+		if (this.crackedPassword.equals("")) {
+			this.log().error("Password " + message.getId() + " could not be cracked");
+		} else {
+			this.log().info("Found password " + this.crackedPassword + " for id: " + message.getId());
+			CrackedPasswordMessage answer = new CrackedPasswordMessage(message.getId(), this.crackedPassword);
+			this.sender().tell(answer, this.self());
+		}
 	}
 
 	private void handle(CurrentClusterState message) {
@@ -99,7 +156,25 @@ public class Worker extends AbstractLoggingActor {
 		if (this.masterSystem.equals(message.member()))
 			this.self().tell(PoisonPill.getInstance(), ActorRef.noSender());
 	}
-	
+
+	private void generateCombinations(List<Character> alphabet, int length, String prefix, int n, String passwordHash)  {
+		if (!this.crackedPassword.equals("")) {
+			return;
+		}
+
+		if (length == 0) {
+			if (hash(prefix).equals(passwordHash)) {
+				this.crackedPassword = prefix;
+			}
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			String newPrefix = (prefix + alphabet.get(i));
+			generateCombinations(alphabet, length - 1, newPrefix, n, passwordHash);
+		}
+	}
+
 	private String hash(String line) {
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -115,17 +190,40 @@ public class Worker extends AbstractLoggingActor {
 			throw new RuntimeException(e.getMessage());
 		}
 	}
-	
+
+	private void checkPermutation(String permutation) {
+		char possibleHint = permutation.charAt(permutation.length() - 1);
+		String currentHintHash = hash(permutation.substring(0, permutation.length() - 1));
+		for (int i = 0; i < this.hintHashes.size(); i++) {
+			if (currentHintHash.equals(this.hintHashes.get(i))) {
+				this.hintHashes.remove(i);
+				this.crackedHints.add(possibleHint);
+				this.log().debug("Cracked hint number " + this.crackedHints.size() + ":" + possibleHint);
+				break;
+			}
+		}
+	}
+
 	// Generating all permutations of an array using Heap's Algorithm
 	// https://en.wikipedia.org/wiki/Heap's_algorithm
 	// https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
-	private void heapPermutation(char[] a, int size, int n, List<String> l) {
+	private void heapPermutation(char[] a, int size, int n, int hintsToCrack) {
+		// If enough hints are found, exit the algorithm
+		if (this.enoughHintsFound) {
+			return;
+		}
+
 		// If size is 1, store the obtained permutation
-		if (size == 1)
-			l.add(new String(a));
+		if (size == 1) {
+			this.checkPermutation(new String(a));
+			if (this.crackedHints.size() >= hintsToCrack) {
+				this.enoughHintsFound = true;
+				return;
+			}
+		}
 
 		for (int i = 0; i < size; i++) {
-			heapPermutation(a, size - 1, n, l);
+			heapPermutation(a, size - 1, n, hintsToCrack);
 
 			// If size is odd, swap first and last element
 			if (size % 2 == 1) {
