2d1ef71432a969a7cf7beb1cf02439408fc56674
Tom Lichtenstein <lichtenstein@uni-potsdam.de>
2020-01-12 19:40:19 +0100

    Refactor variable naming and add comments

--- ddm-idd-scala/src/main/scala/TeamSchlau.scala
+++ ddm-idd-scala/src/main/scala/TeamSchlau.scala
@@ -2,14 +2,13 @@ import java.io.File
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
-
 import scala.collection.immutable.SortedSet
 
-
 object TeamSchlau {
+  // Combination helper methods
+  def createSetFromStringCombiner = (value: (String)) => Set(value)
 
-  def createFromStringCombiner = (value: (String)) => Set(value)
-  def createFromSetCombiner = (value: (Set[String])) => value
+  def createSetFromSetCombiner = (value: (Set[String])) => value
 
   def mergeStringsToSet = (accumulator: Set[String], element: String) =>
     (accumulator + element)
@@ -19,7 +18,6 @@ object TeamSchlau {
 
   def intersectSets = (accumulator1: (Set[String]), accumulator2: (Set[String])) =>
     (accumulator1.intersect(accumulator2))
-
 
   def main(args: Array[String]): Unit = {
     // Parse input arguments
@@ -40,30 +38,30 @@ object TeamSchlau {
     val cores = 4
     val datasetPath = "./data/TPCH"
 
-    // Get all .csv-files for the dataset path
+    // Get all .csv-file paths in the dataset path
     val fileRegex = """.*\.csv$""".r
-    val d = new File(datasetPath)
-    val files = d.listFiles
+    val directory = new File(datasetPath)
+    val files = directory
+      .listFiles
       .filter(_.isFile)
       .filter(file => fileRegex.findFirstIn(file.getName()).isDefined)
       .toList
 
     // Configure Spark session
-    val sparkSession: SparkSession = SparkSession
+    val session: SparkSession = SparkSession
       .builder()
       .appName("Team Schlau")
       .master(s"local[$cores]")
       .getOrCreate()
 
-    sparkSession.conf.set("spark.sql.shuffle.partitions", "16")
-    import sparkSession.implicits._
+    session.conf.set("spark.sql.shuffle.partitions", "16")
+    import session.implicits._
 
     val startTime = System.currentTimeMillis();
 
     // Read .csv-files
     val datasets: Seq[DataFrame] = files.map(file => {
-      sparkSession.read
-        .option("inferSchema", "true")
+      session.read
         .option("header", "true")
         .option("quote", "\"")
         .option("delimiter", ";")
@@ -71,35 +69,55 @@ object TeamSchlau {
     })
 
     // Find inclusion dependencies
-    val columnNameValueTuplesPerDataset:Seq[Dataset[(String, String)]] = datasets.map(dataset => {
+
+    // Get all value, column name tuples for each dataset
+    val datasetColumnNameValueTuples:Seq[Dataset[(String, String)]] = datasets.map(dataset => {
       val columnNames = dataset.columns
       dataset.flatMap(row => {
         row.toSeq.zipWithIndex.map{case(cell, index) => (cell.toString, columnNames(index))}
       })
     })
 
-    val columnNameValueTuples: Dataset[(String, String)] = columnNameValueTuplesPerDataset.reduce((datasetAccumulator, dataset) => datasetAccumulator.union(dataset))
+    // Merge all sequences of tuples
+    val columnNameValueTuples: Dataset[(String, String)] = datasetColumnNameValueTuples
+      .reduce((datasetAccumulator, dataset) => datasetAccumulator.union(dataset))
 
-    val attributeSets:RDD[(Set[String])] = columnNameValueTuples.rdd.combineByKey(createFromStringCombiner, mergeStringsToSet, mergeSets).map(entry => entry._2)
+    // Get all attribute sets
+    val attributeSets:RDD[(Set[String])] = columnNameValueTuples
+      .rdd
+      .combineByKey(createSetFromStringCombiner, mergeStringsToSet, mergeSets)
+      .map(entry => entry._2)
 
-    val inclusionLists:RDD[(String, Set[String])] = attributeSets.flatMap(row => row.toSeq.map(entry => (entry, row.filter(a => !a.equals(entry)))))
+    // Generate subset entries for each entry
+    val inclusionLists:RDD[(String, Set[String])] = attributeSets
+      .flatMap(row => row.toSeq.map(entry => (entry, row.filter(a => !a.equals(entry)))))
 
-    val aggregate:RDD[(String, Set[String])] = inclusionLists.combineByKey(createFromSetCombiner, intersectSets, intersectSets)
+    // Merge all entries by intersection
+    val aggregatedDependencies:RDD[(String, Set[String])] = inclusionLists
+      .combineByKey(createSetFromSetCombiner, intersectSets, intersectSets)
 
-    val sorted:RDD[(String, Set[String])] = aggregate.filter(row => !row._2.isEmpty).sortBy(_._1)
+    // Filter entries with no dependency and sort all entries
+    val sortedDependencies:RDD[(String, Set[String])] = aggregatedDependencies
+      .filter(row => !row._2.isEmpty)
+      .sortBy(_._1)
 
-    sorted.collect().map(entry => {
-      val sortedDependencies = SortedSet[String]() ++ entry._2
-      val dependencyString = sortedDependencies.reduce((acc, entry) => {
-        if (acc.equals("")) {
-          acc + entry
-        } else {
-          acc + ", " + entry
-        }
+    // Format and print the result
+    sortedDependencies
+      .collect()
+      .map(entry => {
+        val sortedDependencies = SortedSet[String]() ++ entry._2
+        val dependencyString = sortedDependencies.reduce((acc, entry) => {
+          if (acc.equals("")) {
+            acc + entry
+          } else {
+            acc + ", " + entry
+          }
+        })
+        println(entry._1 + " < " + dependencyString)
       })
-      println(entry._1 + " < " + dependencyString)
-    })
 
-    println("DONE IN " + (System.currentTimeMillis() - startTime) / 1000 + " s (" + (System.currentTimeMillis() - startTime) / 60000.0 + " min)")
+    println("DONE IN " +
+      (System.currentTimeMillis() - startTime) / 1000.0 + " s (" +
+      (System.currentTimeMillis() - startTime) / 60000.0 + " min)")
   }
 }
